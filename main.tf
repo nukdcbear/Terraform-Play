c49f78e4b876ac33f60dbb84605bd714ee1eb479
David Barringer <david.barringer@relativity.com>
2019-11-15 14:03:30 -0500

    Upgrade to Terraform 0.12

--- main.tf
+++ main.tf
@@ -11,58 +11,63 @@ variable "owner" {
 }
 
 variable "files_dir" {
-    description = "Where the files live"
+  description = "Where the files live"
 }
 
 variable "module_dir" {
-    description = "Where the module lives"
+  description = "Where the module lives"
 }
 
 locals {
-    kv_name = "${var.app}-${var.environment}-${replace(var.owner," ", "-")}-kv"
+  kv_name = "${var.app}-${var.environment}-${replace(var.owner, " ", "-")}-kv"
 
-    kv_short_name = "${var.owner == "Binford Tools" ? "${var.app}-${var.environment}-kv" : length("${var.app}-${var.environment}-${replace(var.owner," ", "-")}-kv") > 24 ? "${substr(var.app, 0, min(length(var.app), 7))}-${var.environment}-${substr("${replace(var.owner," ", "-")}", 0 , min(length("${replace(var.environment," ", "-")}"), 7))}-kv" : "${var.app}-${var.environment}-${replace(var.owner," ", "-")}-kv"}"
+  kv_short_name = var.owner == "Binford Tools" ? "${var.app}-${var.environment}-kv" : length(
+    "${var.app}-${var.environment}-${replace(var.owner, " ", "-")}-kv",
+    ) > 24 ? "${substr(var.app, 0, min(length(var.app), 7))}-${var.environment}-${substr(
+      replace(var.owner, " ", "-"),
+      0,
+      min(length(replace(var.environment, " ", "-")), 7),
+  )}-kv" : "${var.app}-${var.environment}-${replace(var.owner, " ", "-")}-kv"
 
-    kv_name_len = "${length(local.kv_name)}"
+  kv_name_len = length(local.kv_name)
 
-    kv_short_name_len = "${length(local.kv_short_name)}"
+  kv_short_name_len = length(local.kv_short_name)
 
-    module_cwd = "${path.cwd}"
+  module_cwd = path.cwd
 }
-
 
 output "kv_name" {
   description = "Key vault name"
-  value       = "${local.kv_name}"
+  value       = local.kv_name
 }
 
 output "kv_name_len" {
   description = "Key vault name length"
-  value       = "${local.kv_name_len}"
+  value       = local.kv_name_len
 }
-
 
 output "kv_short_name" {
   description = "Key vault short name"
-  value       = "${local.kv_short_name}"
+  value       = local.kv_short_name
 }
 
 output "kv_short_name_len" {
   description = "Key vault short name length"
-  value       = "${local.kv_short_name_len}"
+  value       = local.kv_short_name_len
 }
 
 output "files_dir" {
   description = "Where the files live"
-  value       = "${var.files_dir}"
+  value       = var.files_dir
 }
 
 output "module_dir" {
   description = "Where the module lives"
-  value       = "${var.module_dir}"
+  value       = var.module_dir
 }
 
 output "module_cwd" {
   description = "What the module cwd"
-  value       = "${local.module_cwd}"
+  value       = local.module_cwd
 }
+
